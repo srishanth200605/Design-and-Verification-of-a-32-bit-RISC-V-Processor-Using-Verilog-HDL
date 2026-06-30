bd849357c2d6ef83b5f5eeb694c27e56e1b6a016
AllampallySrishanth <allampallysrishanth@gmail.com>
2026-06-30 19:28:39 +0530

    Add files via upload

---
+++ data_mem.v
@@ -0,0 +1,13 @@
+module data_mem (
+    input clk, mem_read, mem_write,
+    input [31:0] addr, wd,
+    output [31:0] rd
+);
+    reg [31:0] mem [0:255]; // 1KB for simulation
+
+    always @(posedge clk) begin
+        if (mem_write) mem[addr[9:2]] <= wd; // Word aligned
+    end
+
+    assign rd = mem_read ? mem[addr[9:2]] : 32'b0;
+endmodule
