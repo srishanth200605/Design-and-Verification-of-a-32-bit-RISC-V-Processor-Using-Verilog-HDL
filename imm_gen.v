a10764da6fe01d063b7944547eed1ffddcd3a936
AllampallySrishanth <allampallysrishanth@gmail.com>
2026-06-30 19:28:59 +0530

    Add files via upload

---
+++ imm_gen.v
@@ -0,0 +1,13 @@
+module imm_gen (
+    input [31:0] instr,
+    output reg [31:0] imm
+);
+    always @(*) begin
+        case (instr[6:0])
+            7'b0010011, 7'b0000011: imm = {{20{instr[31]}}, instr[31:20]};           // I-type
+            7'b0100011: imm = {{20{instr[31]}}, instr[31:25], instr[11:7]};         // S-type
+            7'b1100011: imm = {{19{instr[31]}}, instr[31], instr[7], instr[30:25], instr[11:8], 1'b0}; // B-type
+            default: imm = 32'b0;
+        endcase
+    end
+endmodule
